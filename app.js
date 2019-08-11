efe6d132739a8ebb66dd6f5045d4c06f5a00e8e5
thecalmbuffalo <53958113+thecalmbuffalo@users.noreply.github.com>
2019-08-11 11:25:56 -0500

    Create app.js

---
+++ app.js
@@ -0,0 +1,9 @@
+var video = document.getElementById('video');
+
+//access camera
+if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+    navigator.mediaDevices.getUserMedia({ video: true }).then(function(stream) {
+        video.srcObject = stream;
+        video.play();
+    });
+}

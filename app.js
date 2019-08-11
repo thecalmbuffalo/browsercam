5a2864d25244aca2a6f178cd317ea60a56087a84
thecalmbuffalo <53958113+thecalmbuffalo@users.noreply.github.com>
2019-08-11 11:29:57 -0500

    Update and rename app.js to index.html

--- app.js
+++
@@ -1,9 +0,0 @@
-var video = document.getElementById('video');
-
-//access camera
-if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
-    navigator.mediaDevices.getUserMedia({ video: true }).then(function(stream) {
-        video.srcObject = stream;
-        video.play();
-    });
-}

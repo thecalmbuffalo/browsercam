e0da8a16482506ad2dde3d41a83d2958c03824cf
thecalmbuffalo <53958113+thecalmbuffalo@users.noreply.github.com>
2019-08-12 04:33:02 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,100 +1,11 @@
-
-if('mediaDevices' in navigator && 'getUserMedia' in navigator.mediaDevices){
-  const stream = await navigator.mediaDevices.getUserMedia({video: true})
+async function getMedia(pc) {
+  let stream = null;
+  
+  try {
+    stream = await navigator.mediaDevices.getUserMedia(constraints);
+    /*use the stream*/
+  } catch(err) {
+    /*handle the error*/
+  }
 }
 
-function hasGetUserMedia() {
-  return !!(navigator.mediaDevices && navigator.mediaDevices.getUserMedia);
-}
-
-if (hasGetUserMedia()) {
-feather.replace();
-
-const controls = document.querySelector('.controls');
-const cameraOptions = document.querySelector('.video-options>select');
-const video = document.querySelector('video');
-const canvas = document.querySelector('canvas');
-const screenshotImage = document.querySelector('img');
-const buttons = [...controls.querySelectorAll('button')];
-let streamStarted = true;
-
-const [play, pause, screenshot] = buttons;
-
-const constraints = {
-  {
-  video: {
-    width: 1280,
-    height: 720,
-  }
-},
-    height: {
-      min: 400,
-      ideal: 1080,
-      max: 1440,
-    },
-  }
-};
-
-cameraOptions.onchange = () => {
-  const updatedConstraints = {
-    ...constraints,
-    deviceId: {
-      exact: cameraOptions.value
-    }
-  };
-
-  startStream(updatedConstraints);
-};
-
-play.onclick = () => {
-  if (streamStarted) {
-    video.play();
-    play.classList.add('d-none');
-    return;
-  }
-  if ('mediaDevices' in navigator && navigator.mediaDevices.getUserMedia) {
-    const updatedConstraints = {
-      ...constraints,
-      deviceId: {
-        exact: cameraOptions.value
-      }
-    };
-    startStream(updatedConstraints);
-  }
-};
-
-const pauseStream = () => {
-  video.pause();
-  play.classList.remove('d-none');
-  pause.classList.add('d-none');
-};
-
-pause.onclick = pauseStream;
-screenshot.onclick = doScreenshot;
-
-const startStream = async (constraints) => {
-  const stream = await navigator.mediaDevices.getUserMedia(constraints);
-  handleStream(stream);
-};
-
-const handleStream = (stream) => {
-  video.srcObject = stream;
-  play.classList.add('d-none');
-  pause.classList.remove('d-none');
-  screenshot.classList.remove('d-none');
-
-};
-
-const getCameraSelection = sync () => {
-  const devices = await navigator.mediaDevices.enumerateDevices();
-  const videoDevices = devices.filter(device => device.kind === 'videoinput');
-  const options = videoDevices.map(videoDevice => {
-    return '<option value="${videoDevice.deviceId}">${videoDevice.label}</option>';
-  });
-  cameraOptions.innerHTML = options.join('');
-};
-
-getCameraSelection();
-} else {
-  alert('getUserMedia() is not supported by your browser');
-}
